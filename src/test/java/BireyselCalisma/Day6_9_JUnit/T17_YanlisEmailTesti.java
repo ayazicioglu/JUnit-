afa543a75237ade2fbeb2a489504138da19561a3
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-03-10 15:45:44 +0300

    10.03.23

---
+++ src/test/java/BireyselCalisma/Day6_9_JUnit/T17_YanlisEmailTesti.java
@@ -0,0 +1,17 @@
+package BireyselCalisma.Day6_9_JUnit;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class T17_YanlisEmailTesti extends TestBase {
+
+    @Test
+    public void test01(){
+        //2. http://automationpractice.com/index.php sayfasina gidelim
+        driver.get("http://automationpractice.com/index.php"); //SİTE DEĞİŞMİŞ! DEVAM EDEMİYORUM
+
+        //3. Sign in butonuna basalim
+        //4. Email kutusuna @isareti olmayan bir mail yazip enter’a bastigimizda “Invalid
+        //email address” uyarisi ciktigini test edelim
+    }
+}
